d3fef3133156dc9340c56f1857aec993fce929da
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-10-19 11:19:22 -0500

    state context added and get requests from json server added

--- src/App.js
+++ src/App.js
@@ -4,36 +4,54 @@ import DeleteTodo from "./DeleteTodo";
 import ToggleTodo from "./ToggleTodo";
 import TodoList from "./TodoList";
 import appReducer from './reducers';
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { StateContext } from './contexts'
+import { useResource } from 'react-request-hook'
 
 function App() {
 
-  const initialPosts = [
-    {
-      title: "Complete CSC 436 Todo App",
-      description: "Due on Tuesday!",
-      dateCreated: Date(Date.now()).toString(),
-      completed: false,
-      dateCompleted: ''
-    },
-    {
-      title: "Wash Clothes",
-      description: "Don't forget to dry them!",
-      dateCreated: Date(Date.now()).toString(),
-      completed: false,
-      dateCompleted: ''
-    },
-    {
-      title: "Do Groceries",
-      description: "Get healthier foods",
-      dateCreated: Date(Date.now()).toString(),
-      completed: false,
-      dateCompleted: ''
-    },
-  ]
+  // const initialPosts = [
+  //   {
+  //     title: "Complete CSC 436 Todo App",
+  //     description: "Due on Tuesday!",
+  //     dateCreated: Date(Date.now()).toString(),
+  //     completed: false,
+  //     dateCompleted: ''
+  //   },
+  //   {
+  //     title: "Wash Clothes",
+  //     description: "Don't forget to dry them!",
+  //     dateCreated: Date(Date.now()).toString(),
+  //     completed: false,
+  //     dateCompleted: ''
+  //   },
+  //   {
+  //     title: "Do Groceries",
+  //     description: "Get healthier foods",
+  //     dateCreated: Date(Date.now()).toString(),
+  //     completed: false,
+  //     dateCompleted: ''
+  //   },
+  // ]
 
-  const [state, dispatch] = useReducer(appReducer, { user: '', todos: initialPosts })
+  const [state, dispatch] = useReducer(appReducer, { user: '', todos: [] })
+
+  const [todos, getTodos] = useResource(() => ({
+    url: '/todos',
+    method: 'get'
+  }))
+
+  useEffect(getTodos, [])
+
+  useEffect(() => {
+    if (todos && todos.data) {
+      dispatch({ type: 'FETCH_TODOS', todos: todos.data })
+      console.log("WE HAVE DATA")
+    }
+  }, [todos])
+
+
+  
   const { user } = state;
 
   return (
