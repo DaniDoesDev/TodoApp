b8bc42123c5eccb53256fa46a821e9dd48c90bf0
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-10-19 18:26:36 -0500

    add useResource hook for get/post requests for Login & Register

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,22 +1,42 @@
-import React, { useContext, useState } from 'react';
-
+import React, { useContext, useState, useEffect } from 'react';
+import { useResource } from 'react-request-hook'
 import { StateContext } from '../contexts';
 
 export default function Login() {
 
-    const {dispatch} = useContext(StateContext)
+    const { dispatch } = useContext(StateContext)
 
     const [username, setUsername] = useState('');
+    const [loginFailed, setLoginFailed] = useState(false)
+    const [password, setPassword] = useState('')
 
     function handleUsername(evt) { setUsername(evt.target.value) }
+    function handlePassword(evt) { setPassword(evt.target.value) }
+
+    const [user, login] = useResource((username, password) => ({
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            if (user.data.length > 0) {
+                setLoginFailed(false)
+                dispatch({ type: 'LOGIN', username: user.data[0].username })
+            } else {
+                setLoginFailed(true)
+            }
+        }
+    }, [user])
 
     return (
-        <form onSubmit={e => { e.preventDefault(); dispatch({ type: 'LOGIN', username }); }}>
+        <form onSubmit={e => { e.preventDefault(); login(username, password) }}>
             <label htmlFor="login-username">Username:</label>
             <input type="text" name="login-username" id="login-username" value={username} onChange={handleUsername} />
             <label htmlFor="login-password">Password:</label>
-            <input type="password" name="login-password" id="login-password" />
+            <input type="password" value={password} onChange={handlePassword} name="login-password" id="login-password" />
             <input type="submit" value="Login" />
+            {loginFailed && <span style={{ color: 'red' }}>Invalid username or password</span>}
         </form>
     )
 }
--- src/user/Register.js
+++ src/user/Register.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-
+import React, { useContext, useState, useEffect } from 'react';
+import { useResource } from 'react-request-hook'
 import { StateContext } from '../contexts';
 
 export default function Register() {
@@ -12,8 +12,20 @@ export default function Register() {
         passwordRepeat: ""
     })
 
+    const [ user, register ] = useResource((username, password) => ({
+        url: '/users',
+        method: 'post',
+        data: { username, password }
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            dispatch({ type: 'REGISTER', username: user.data.username })
+        }
+    }, [user])
+
     return (
-        <form onSubmit={e => { e.preventDefault(); dispatch({ type: 'REGISTER', username: formData.username, password: formData.password, passwordRepeat: formData.passwordRepeat }); }}>
+        <form onSubmit={e => { e.preventDefault(); register(formData.username, formData.password) }}>
             <label htmlFor="register-username">Username:</label>
             <input type="text" value={formData.username} onChange={e => setFormData({ ...formData, username: e.target.value })} name="register-username" id="register-username" />
 
