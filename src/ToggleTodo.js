eb5481a8d5625cca997e04a079fe0274a90cf419
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-10-21 21:16:58 -0500

    bug fix for toggle todo

--- src/ToggleTodo.js
+++ src/ToggleTodo.js
@@ -1,10 +1,10 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { StateContext } from './contexts'
 import { useResource } from 'react-request-hook'
 
 export default function ToggleTodo() {
 
-    const {state, dispatch} = useContext(StateContext)
+    const { state, dispatch } = useContext(StateContext)
     const [id, setId] = useState()
     const [completed, setCompleted] = useState()
     const [dateCompleted, setDateCompleted] = useState()
@@ -13,24 +13,33 @@ export default function ToggleTodo() {
 
     function handleId(evt) { setId(evt.target.value) }
 
-    const [todo , toggleTodo ] = useResource(({ id , completed, dateCompleted }) => ({
+    const [todo, toggleTodo] = useResource(({ id, completed, dateCompleted }) => ({
         url: `/todos/${parseInt(id)}`,
         method: 'patch',
-        data: { id, completed, dateCompleted } 
+        data: { id, completed, dateCompleted }
     }))
 
-     function handleToggle( targetId ) {
-        todos.forEach(function (todo) {
-            if (todo.id === targetId) {
-                setCompleted(todo.completed);
-                setDateCompleted(todo.dateCompleted);
-                toggleTodo( {id, completed, dateCompleted });
-            }
-          });
-     }
+    function handleToggle(targetId) {
+        const todoToUpdate = todos.find(todo => todo.id === targetId)
+
+        todoToUpdate.completed = !todoToUpdate.completed
+        if (todoToUpdate.completed) {
+            todoToUpdate.dateCompleted = Date(Date.now()).toString();
+        } else {
+            todoToUpdate.dateCompleted = "";
+        }
+        
+        toggleTodo({ ...todoToUpdate });
+    }
+
+    useEffect(() => {
+        if (todo && todo.data && todo.isLoading === false) {
+            dispatch({type: "TOGGLE_TODO", id: todo.id, completed: todo.completed, dateCompleted: todo.dateCompleted });
+        }
+    }, [todo])
 
     return (
-        <form onSubmit={e => { e.preventDefault(); dispatch({ type: "TOGGLE_TODO", id: parseInt(id) }); handleToggle(parseInt(id)) }}>
+        <form onSubmit={e => { e.preventDefault(); handleToggle(parseInt(id)) }}>
 
             <div>
                 <label htmlFor="toggle-id">Todo Item ID To Be Toggled:</label>
