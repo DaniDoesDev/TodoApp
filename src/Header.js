fed2a6276fec334cb26a981ede849b1f89e75b6e
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-11-15 16:11:24 -0600

    make todo card

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import { Navbar } from 'react-bootstrap'
 import { Link } from 'react-navi'
 
 const Header = ({text}) => {     
-     return <Link href="/"><Navbar.Brand style={{color: 'deepskyblue'}}>{text}</Navbar.Brand></Link>
+     return <Link href="/"><Navbar.Brand style={{color: 'darkslateblue'}}>{text}</Navbar.Brand></Link>
 }
 
 export default Header 
--- src/Todo.js
+++ src/Todo.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import { Link } from 'react-navi'
+import { Card } from 'react-bootstrap'
 
 export default function Todo({ title, description, dateCreated, completed, dateCompleted, id, author }) {
 
-
     return (
-        <div>
-            <h3>{title}</h3>
-            <div>{description}</div>
-            <div>Todo ID: {id}</div>
-            <div>Written By: {author}</div>
-            <div> Date created: {dateCreated} </div>
-            <div>
-                <label> Completed?
-                    <input type="checkbox" name="box-id" value={completed} checked={completed} onChange={e => {
-                        completed = !completed;
-                    }} />
-                </label>
-            </div>
-            <div> {completed && <text> Date completed: {dateCompleted} </text>}</div>
-            <br />
-        </div>
+        <Card>
+            <Card.Body>
+                <Card.Title>{title}
+                </Card.Title>
+                <Card.Subtitle>
+                    <i>Written by <b>{author}</b></i>
+                </Card.Subtitle>
+                <Card.Text>
+                    <div>{description}</div>
+                    <div>Todo ID: {id}</div>
+                    <div> Date created: {dateCreated} </div>
+                    <div>
+                        <label> Completed?
+                            <input type="checkbox" name="box-id" value={completed} checked={completed} onChange={e => {
+                                completed = !completed;
+                            }} />
+                        </label>
+                    </div>
+                    <div> {completed && <text> Date completed: {dateCompleted} </text>}</div>
+                </Card.Text>
+                <Link href={`/users/${author}`}>View author page</Link>
+            </Card.Body>
+        </Card>
+
     )
 }
+
+
